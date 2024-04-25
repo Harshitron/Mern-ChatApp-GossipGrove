b5392fb0d93dae609fa0b63993a043d56a28091c
Harshit Sachdeva <harshitsachdeva291104@gmail.com>
2024-04-25 10:22:29 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 
@@ -13,6 +15,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname  = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -22,11 +26,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "frontend/dist")))
 
-/* app.get('/', (req,res)=>{
-    res.send("Hello Word!");
-}) */
-
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, ()=>{
     connectToMongoDB();
