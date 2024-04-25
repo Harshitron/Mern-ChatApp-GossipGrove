15cf1e87539e7214560e47fbc8bad95a8ff31ab7
Harshit Sachdeva <119051777+harshit-bpit@users.noreply.github.com>
2024-04-25 11:24:14 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async ()=>{
     try {
-        await mongoose.connect(process.env.URI)
+        await mongoose.connect(process.env.MONGO_DB_URI)
         console.log("Connected to MONGO DB")
     } catch (error) {
         console.log("Error Connecting to DB ", error.message)
